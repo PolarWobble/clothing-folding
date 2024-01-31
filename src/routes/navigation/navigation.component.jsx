06bf0e409ab02f66272218c31414f36ac9c734fe
Johan Fors <johan.fors.90@gmail.com>
2024-01-31 11:00:49 +0100

    added react router, navbar, dummy shop page and initial styling for navbar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from 'react';
+import {Outlet, Link} from 'react-router-dom';
+
+import {ReactComponent as ClothingFoldingLogo} from '../../assets/clothes-fold.svg';
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return(
+      <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <ClothingFoldingLogo className='logo' />
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    );
+  };
+
+export default Navigation;
