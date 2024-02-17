7e85d29dcc2831109ac57f89b912fa1d0ccc99b8
Johan Fors <johan.fors.90@gmail.com>
2024-02-17 20:28:47 +0100

    added styled-components and updated navigation.styles with s-c

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,7 +9,7 @@ import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 
-import './navigation.styles.scss'
+import {NavigationContainer, LogoContainer, NavLinks, NavLink} from './navigation.styles.jsx';
 
 const Navigation = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -17,24 +17,24 @@ const Navigation = () => {
 
   return(
     <Fragment>
-      <div className='navigation'>
-          <Link className='logo-container' to='/'>
+      <NavigationContainer>
+          <LogoContainer to='/'>
               <ClothingFoldingLogo className='logo' />
-          </Link>
-          <div className='nav-links-container'>
-              <Link className='nav-link' to='/shop'>
+          </LogoContainer>
+          <NavLinks>
+              <NavLink to='/shop'>
                   SHOP
-              </Link>
+              </NavLink>
               {
                 currentUser ? 
-                  <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
+                  <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
                 : 
-                  <Link className='nav-link' to='/auth'>SIGN IN</Link>
+                  <NavLink to='/auth'>SIGN IN</NavLink>
               }
               <CartIcon />
-          </div>
+          </NavLinks>
           {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
